980ccb9703b0dd8a55e0387b6c9be686705183de
kimheegu <chkorea11@gmail.com>
2024-06-19 17:40:10 +0900

    Add files via upload

---
+++ news7/src/main/java/com/example/news7/News7Application.java
@@ -0,0 +1,13 @@
+package com.example.news7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class News7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(News7Application.class, args);
+	}
+
+}
---
+++ news7/src/test/java/com/example/news7/News7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class News7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
